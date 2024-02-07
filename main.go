aea163ad3d404ead5585ab53120d02cdb6e159d0
TecLegacy <kshvkumar.kk2@gmail.com>
2024-02-08 01:15:48 +0530

    Dotenv configure
    Added joho/godotenv package to laod .env for active session

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("port not found")
+	}
 
 	fmt.Print("HelloWord")
 }
