019309bb6505c642b5116eb7f90a7d510a601c60
TecLegacy <kshvkumar.kk2@gmail.com>
2024-02-13 19:02:03 +0530

    Feeds Migration and query
    Create feed which stores url of RSS endpoints in DB

--- internal/database/models.go
+++ internal/database/models.go
@@ -10,6 +10,15 @@ import (
 	"github.com/google/uuid"
 )
 
+type Feed struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string
+	Url       string
+	UsersID   uuid.UUID
+}
+
 type User struct {
 	ID        uuid.UUID
 	CreatedAt time.Time
---
+++ sql/queries/feeds.sql
@@ -0,0 +1,6 @@
+-- name: CreateFeed :one
+
+INSERT INTO feeds(id,created_At,updated_At,name,url,users_id)
+VALUES($1,$2,$3,$4,$5,$6)
+RETURNING *;
+
